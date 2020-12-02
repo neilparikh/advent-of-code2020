cc6a1ec7e19c79cf00af1e6f7a3aca4f2bcfa7ec
Neil Parikh <n3parikh@csclub.uwaterloo.ca>
2020-12-02 01:24:39 -0500

    day 2

---
+++ day2.hs
@@ -0,0 +1,48 @@
+{-# OPTIONS_GHC -Wall #-}
+import Text.Parsec hiding (count)
+import Data.Either (isRight, rights)
+import Control.Monad (unless)
+
+main :: IO ()
+main = do
+  rawInput <- fmap lines getContents
+  let parsedInput' = fmap (applyParser entryParser) rawInput
+  unless (all isRight parsedInput') (error "parse error")
+  let parsedInput = rights parsedInput'
+  -- Part 1
+  print . length . filter isValid1 $ parsedInput
+  -- Part 2
+  print . length . filter isValid2 $ parsedInput
+
+data Policy = Policy Int Int Char
+
+isValid2 :: (Policy, String) -> Bool
+isValid2 (Policy i j c, password) = (password !! (i - 1) == c) `xor` (password !! (j - 1) == c)
+  where
+  xor a b = (a && not b) || (b && not a)
+
+isValid1 :: (Policy, String) -> Bool
+isValid1 (Policy lowerBound upperBound c, password) = count >= lowerBound && count <= upperBound
+  where
+  count = length . filter (== c) $ password
+
+type Parser a = Parsec String () a
+
+applyParser :: Parser a -> String -> Either ParseError a
+applyParser parser = runParser parser () ""
+
+entryParser :: Parser (Policy, String)
+entryParser = do
+  policy <- policyParser
+  _ <- string ": "
+  password <- many1 letter
+  return (policy, password)
+
+policyParser :: Parser Policy
+policyParser = do
+  i <- fmap read (many1 digit)
+  _ <- char '-'
+  j <- fmap read (many1 digit)
+  _ <- char ' '
+  c <- letter
+  return $ Policy i j c
