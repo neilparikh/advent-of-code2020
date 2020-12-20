32e4d522546462b7f665b15cc8537ba6e4d0465f
Neil Parikh <n3parikh@csclub.uwaterloo.ca>
2020-12-20 05:22:58 -0500

    day 13 part 2

--- day13.hs
+++ day13.hs
@@ -1,5 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
-import Data.List (minimumBy)
+import Data.List (minimumBy, foldl1')
 import Data.Ord (comparing)
 import Control.Arrow ((&&&), second)
 
@@ -11,12 +11,24 @@ main = do
   let ids1 = fmap read . filter (/= "x") $ rawIds
   print . uncurry (*) . minimumBy (comparing snd) . fmapWithTag (timeToBus time) $ ids1
   -- part 2
-  let ids2 = fmap (second (read :: String -> Int)) . filter ((/= "x") . snd) . zip [0..] $ rawIds
-  print ids2
-  return ()
+  let ids2 = fmap ((\(a, n) -> (n - a, n)) . second read) . filter ((/= "x") . snd) . zip [0..] $ rawIds
+  print . uncurry mod . foldl1' solve2 $ ids2
 
-timeToBus :: Int -> Int -> Int
+timeToBus :: Integer -> Integer -> Integer
 timeToBus time bus = bus - (time `mod` bus)
+
+solve2 :: (Integer, Integer) -> (Integer, Integer) -> (Integer, Integer)
+solve2 (a1, n1) (a2, n2) = let (m1, m2) = egcd n1 n2
+                               n' = n1 * n2
+                               a' = (a1 * m2 * n2 + a2 * m1 * n1) `mod` n'
+                           in (a', n')
+
+egcd :: Integer -> Integer -> (Integer, Integer)
+egcd _ 0 = (1, 0)
+egcd a b = (t, s - q * t)
+  where
+  (q, r) = quotRem a b
+  (s, t) = egcd b r
 
 wordsBy :: (a -> Bool) -> [a] -> [[a]]
 wordsBy f lst = go lst []
